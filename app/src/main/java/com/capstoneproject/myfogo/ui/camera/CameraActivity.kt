871f32fa57277c79c48e9a7dcc91684984f63d48
farizaiz <farizalghifari74@gmail.com>
2022-06-01 00:05:23 +0700

    Upload files

---
+++ app/src/main/java/com/capstoneproject/myfogo/ui/camera/CameraActivity.kt
@@ -0,0 +1,12 @@
+package com.capstoneproject.myfogo.ui.camera
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstoneproject.myfogo.R
+
+class CameraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+    }
+}
