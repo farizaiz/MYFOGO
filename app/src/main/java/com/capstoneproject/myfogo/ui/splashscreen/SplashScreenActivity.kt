0ca51b59f828ab77dc3acc598fca8febee1d6257
farizaiz <farizalghifari74@gmail.com>
2022-06-05 22:15:51 +0700

    Update Login,Register with Firebase Authentication & update SplashScreenActivity.kt

--- app/src/main/java/com/capstoneproject/myfogo/ui/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/capstoneproject/myfogo/ui/splashscreen/SplashScreenActivity.kt
@@ -1,12 +1,23 @@
 package com.capstoneproject.myfogo.ui.splashscreen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.capstoneproject.myfogo.R
+import com.capstoneproject.myfogo.ui.login.LoginActivity
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(applicationContext, LoginActivity::class.java))
+            finish()
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        }, 3000L)
     }
 }
