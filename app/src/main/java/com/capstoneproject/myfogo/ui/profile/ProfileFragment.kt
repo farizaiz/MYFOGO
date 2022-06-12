ce82fcc5adcf9167b08a2ab685da7c6181a81e4e
farizaiz <farizalghifari74@gmail.com>
2022-06-12 11:29:54 +0700

    Update ProfileFragment.kt & fragment_profile.xml

--- app/src/main/java/com/capstoneproject/myfogo/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/capstoneproject/myfogo/ui/profile/ProfileFragment.kt
@@ -1,24 +1,31 @@
 package com.capstoneproject.myfogo.ui.profile
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.capstoneproject.myfogo.databinding.FragmentProfileBinding
 import com.capstoneproject.myfogo.ui.login.LoginActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.storage.FirebaseStorage
+import com.squareup.picasso.Picasso
+import java.io.ByteArrayOutputStream
 
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
     lateinit var auth: FirebaseAuth
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    lateinit var imgUri: Uri
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -26,16 +33,11 @@ class ProfileFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val userViewModel =
+
             ViewModelProvider(this).get(ProfileViewModel::class.java)
 
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textProfile
-        userViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
@@ -43,9 +45,108 @@ class ProfileFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
+
+        binding.imgProfile.setOnClickListener {
+            goToCamera()
+        }
 
         binding.btnLogout.setOnClickListener {
             logout()
+        }
+
+        if (user != null) {
+            binding.edtEmail.setText(user.email)
+            binding.edtName.setText(user.displayName)
+
+            if (user.photoUrl != null) {
+                Picasso.get().load(user.photoUrl).into(binding.imgProfile)
+            } else {
+                Picasso.get().load("https://bit.ly/3wc3Lcn").into(binding.imgProfile)
+            }
+        }
+
+
+        binding.btnSave.setOnClickListener saveProfile@{
+
+            val image = when {
+                ::imgUri.isInitialized -> imgUri
+                user?.photoUrl == null -> Uri.parse("https://bit.ly/3wc3Lcn")
+                else -> user.photoUrl
+            }
+
+            val name = binding.edtName.text.toString()
+
+            if (name.isEmpty()) {
+                binding.edtName.error = "Nama Belum Di isi"
+                binding.edtName.requestFocus()
+                return@saveProfile
+            }
+
+            //update disini
+            UserProfileChangeRequest.Builder()
+                .setDisplayName(name)
+                .setPhotoUri(image)
+                .build().also {
+                    user?.updateProfile(it)?.addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            val toast = Toast.makeText(
+                                activity,
+                                "Data Profile Berhasil Disimpan !",
+                                Toast.LENGTH_SHORT
+                            )
+                            toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0)
+                            toast.show()
+                        } else {
+                            Toast.makeText(
+                                activity,
+                                "${task.exception?.message}",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+
+                    }
+
+                }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQ_CAM && resultCode == Activity.RESULT_OK) {
+            val imgBitmap = data?.extras?.get("data") as Bitmap
+
+            uploadImgToFirebase(imgBitmap)
+        }
+    }
+
+    private fun uploadImgToFirebase(imgBitmap: Bitmap) {
+        val baos = ByteArrayOutputStream()
+        val ref =
+            FirebaseStorage.getInstance().reference.child("img_user/${FirebaseAuth.getInstance().currentUser?.email}")
+        imgBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val img = baos.toByteArray()
+        ref.putBytes(img)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    ref.downloadUrl.addOnCompleteListener { Task ->
+                        Task.result?.let { Uri ->
+                            imgUri = Uri
+                            binding.imgProfile.setImageBitmap(imgBitmap)
+                        }
+                    }
+                }
+            }
+    }
+
+    private fun goToCamera() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { intent ->
+
+            activity?.packageManager?.let {
+                intent?.resolveActivity(it).also {
+                    startActivityForResult(intent, REQ_CAM)
+                }
+            }
         }
     }
 
@@ -60,5 +161,9 @@ class ProfileFragment : Fragment() {
         val i = Intent(context, LoginActivity::class.java)
         startActivity(i)
         activity?.finish()
+    }
+
+    companion object {
+        const val REQ_CAM = 100
     }
 }
